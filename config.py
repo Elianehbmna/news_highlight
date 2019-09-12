239c1d64497dd816245a0d345972a4becda0541e
Elianehbmna <elianehabimana3@gmail.com>
2019-09-12 15:05:52 +0200

    Creating First API call

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
- SOURCE_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
- ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+ SOURCE_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+#  ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
  SOURCE_API_KEY  = os.environ.get('SOURCE_API_KEY ')
 
 class ProdConfig(Config):
