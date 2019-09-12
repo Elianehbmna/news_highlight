911a9f1650e6c3b6dce8d5de605f4c36c5d3af48
Elianehbmna <elianehabimana3@gmail.com>
2019-09-12 12:14:34 +0200

    dynamic route

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,12 @@ from flask import render_template
 from . import main
 
 # Views
-@main.route('/')
-def index():
+@main.route('/source/<int:source_id>')
+def index(source_id):
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    
+    return render_template('source.html',id = source_id)
